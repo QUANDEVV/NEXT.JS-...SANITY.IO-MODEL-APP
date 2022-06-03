5e73a5fc442097a2a372131572902d34d2685a02
web3Codess <vercelploy@gmail.com>
2022-06-03 21:31:27 +0300

    commit on 'origin'

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
     {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]}  /> */}
 
    <div className='products-heading'>
-     <h2>Best Selling</h2>
+     <h2>MODEL FANS</h2>
      <p>Speakers of Many Variations</p>
    </div>
 
