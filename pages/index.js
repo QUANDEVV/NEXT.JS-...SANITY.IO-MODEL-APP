7c3759b5c7af222172eef715c6c5dd954ad81ad9
web3Codess <vercelploy@gmail.com>
2022-06-03 21:26:07 +0300

    commit on 'origin'

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { client } from '../lib/client'
 
 const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]}  /> */}
 
    <div className='products-heading'>
      <h2>Best Selling</h2>
