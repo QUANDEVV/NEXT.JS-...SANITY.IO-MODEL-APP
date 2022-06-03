16a72d8f676549f7296b35f164c82379a6917996
web3Codess <vercelploy@gmail.com>
2022-06-03 22:19:31 +0300

    commit on 'origin'

--- components/Product.js
+++ components/Product.js
@@ -20,7 +20,8 @@ const Product = (  { product: { image, name , slug , price} }) => {
 <div>
 
 
-  {/* <Link href={`/product/${slug.current}`}>  */}
+  <Link href={`/product/${slug.current}`}> 
+
   <div className='product-card'>
 <img   
 src={urlFor(image && image[0])}
@@ -30,7 +31,7 @@ className="product-image"
 {/* <p className='product-name'>{name}</p> */}
 {/* <p className='product-price'>{price}</p> */}
 </div>
-{/* </Link> */}
+</Link>
 
   <div className={style.likesContainer}>
     <AiOutlineHeart  className={style.heartIcon}/>
